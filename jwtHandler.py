0fac8010d3b72cd175314c24380718fe4ddc5eef
M N Tushar <45463161+mntushar@users.noreply.github.com>
2024-12-02 15:54:11 +0600

    update

--- jwtHandler.py
+++ jwtHandler.py
@@ -2,11 +2,15 @@ import jwt
 from django.conf import settings
 import datetime
 
+from library.models.response import Response
+
+
 class JWTHandler:
     __algorithm = settings.SETTING.JWT_ENCODE_ALGORITHM
     __private_key = settings.SETTING.JWT_PRIVATE_KEY
     __public_key = settings.SETTING.JWT_PUBLIC_KEY
     __token_validation_time = int(settings.SETTING.JWT_TOKEN_VALIDATION_TIME)
+    __response = Response()
 
     def __generate(self, payload: dict) -> str:
         try:
@@ -20,21 +24,27 @@ class JWTHandler:
         if aud is not None:
             claims['aud'] = aud
 
-        claims['iat'] = str(int(datetime.datetime.now().timestamp()))
-        claims['exp'] = str(int((datetime.datetime.now() + datetime
-                             .timedelta(seconds=self.__token_validation_time)).timestamp()))
+        claims['iat'] = int(datetime.datetime.now().timestamp())
+        claims['exp'] = int((datetime.datetime.now() + datetime
+                             .timedelta(seconds=self.__token_validation_time)).timestamp())
 
         token = self.__generate(claims)
         return token
 
-    def verify_token(self, token: str):
+    def verify_token(self, token: str) -> Response:
         try:
             payload = jwt.decode(token, self.__public_key, algorithms=[self.__algorithm])
-            return payload
-        except jwt.ExpiredSignatureError:
-            return False
-        except jwt.InvalidTokenError:
-            return False
+            self.__response.is_success = True
+            self.__response.message = payload
+            return self.__response
+        except jwt.ExpiredSignatureError as e:
+            self.__response.is_success = False
+            self.__response.message = e
+            return self.__response
+        except jwt.InvalidTokenError as e:
+            self.__response.is_success = False
+            self.__response.message = e
+            return self.__response
 
     def decode_token(self, token: str) -> dict:
         return  jwt.decode(token, options={'verify_signature': False, 'verify_exp': False})
